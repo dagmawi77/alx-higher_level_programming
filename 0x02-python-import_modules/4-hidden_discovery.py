32a797caec0f26c40fd58e440db54cbe61b37f19
Dagmawi Letarik <dagmawiletarik@gmail.com>
2022-12-01 12:01:55 +0300

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from variable_load_5 import a
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
 
-    print(a)
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
