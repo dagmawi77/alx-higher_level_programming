d2722fabf9929707e0dbc5464a365a63eadbcb71
Dagmawi Letarik <dagmawiletarik@gmail.com>
2022-12-03 10:29:00 +0300

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    """Print a matrix of integers."""
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
                 print("{:d}".format(matrix[i][j]), end="")
