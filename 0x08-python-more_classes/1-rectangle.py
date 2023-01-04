76eab5a373d1afdc8340953546e4909d58287364
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-01-04 23:07:53 +0300

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,14 +4,12 @@
 
 
 class Rectangle:
-
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        
         return self.__width
 
     @width.setter
