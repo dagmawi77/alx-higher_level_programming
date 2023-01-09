becfc8b63b905c67bb1cf84c687dbd06217c0164
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-01-09 21:41:40 +0300

    Create 7-base_geometry.py

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Defines a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Reprsent base geometry."""
+
+    def area(self):
+        """Not yet implemented."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate a parameter as an integer.
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
