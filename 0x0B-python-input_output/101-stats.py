1c47e5dabfc9a100141d71ed361176a522368b15
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-01-10 22:39:13 +0300

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
-"""Reads from standard input and computes metrics."""
+"""Reads from standard input and computes metrics.
+"""
 
 
 def print_stats(size, status_codes):
-    """Print accumulated metrics."""
+    """Print accumulated metrics.
+    """
     print("File size: {}".format(size))
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
