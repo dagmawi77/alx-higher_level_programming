dd764fbb58dcd6204f3975428fc37028fc94f905
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-01-16 21:54:55 +0300

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-# square.py
-# Brennan D Baraban <375@holbertonschool.com>
+
 """Defines a square class."""
 from models.rectangle import Rectangle
 
@@ -10,11 +9,6 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize a new Square.
-        Args:
-            size (int): The size of the new Square.
-            x (int): The x coordinate of the new Square.
-            y (int): The y coordinate of the new Square.
-            id (int): The identity of the new Square.
         """
         super().__init__(size, size, x, y, id)
 
@@ -30,13 +24,6 @@ class Square(Rectangle):
 
     def update(self, *args, **kwargs):
         """Update the Square.
-        Args:
-            *args (ints): New attribute values.
-                - 1st argument represents id attribute
-                - 2nd argument represents size attribute
-                - 3rd argument represents x attribute
-                - 4th argument represents y attribute
-            **kwargs (dict): New key/value pairs of attributes.
         """
         if args and len(args) != 0:
             a = 0
