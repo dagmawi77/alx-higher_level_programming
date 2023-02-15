80e52f2cfd564a4d79a5d71ee6be773a7c74e607
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-02-15 22:35:03 +0300

    Update 15-groups.sql

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Group by Statment
-SELECT score, COUNT(score) AS number FROM second_table GROUP BY score;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER by number DESC;
