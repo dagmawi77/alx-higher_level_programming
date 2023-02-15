efeddabb1f9b9c59e7b8362ee73086a20168e4e5
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-02-15 23:48:13 +0300

    Update 101-not_a_comedy.sql

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,4 +1,5 @@
--- Lists all shows without the comedy genre .
+-- Lists all shows without the comedy genre in the database hbtn_0d_tvshows.
+SELECT DISTINCT title
   FROM tv_shows AS t
        LEFT JOIN tv_show_genres AS s
        ON s.show_id = t.id
@@ -10,7 +11,6 @@
                 FROM tv_shows AS t
 	             INNER JOIN tv_show_genres AS s
 		     ON s.show_id = t.id
-
 		     INNER JOIN tv_genres AS g
 		     ON g.id = s.genre_id
 		     WHERE g.name = "Comedy")
