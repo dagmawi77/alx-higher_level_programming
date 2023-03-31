1efbd2f7981b70b3c2a7582db6e2a63f3d61c341
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-03-31 22:33:08 +0300

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """Lists the 10 most recent commits on a given GitHub repository.
-
-Usage: ./100-github_commits.py <repository name> <repository owner>
 """
 import sys
 import requests
