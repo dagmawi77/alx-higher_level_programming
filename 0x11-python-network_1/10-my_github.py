ff7352252fee10e9314415f706ce2adf8b5c8b94
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-03-31 22:28:46 +0300

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 """Uses the GitHub API to display a GitHub ID based on given credentials.
-
-Usage: ./10-my_github.py <GitHub username> <GitHub password>
-  - Uses Basic Authentication to access the ID.
 """
 import sys
 import requests
