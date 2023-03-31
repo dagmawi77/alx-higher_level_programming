c02e9eef938e95e571d7bd3b3018c6f5785a4b6d
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-03-31 22:20:34 +0300

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 """Sends a request to a given URL and displays the response body.
-
-Usage: ./3-error_code.py <URL>
-  - Handles HTTP errors.
 """
 import sys
 import urllib.error
