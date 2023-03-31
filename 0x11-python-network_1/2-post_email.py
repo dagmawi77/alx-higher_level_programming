15a54e11aecbb820ac65f4612d8c3742f198942f
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-03-31 22:19:13 +0300

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 """Sends a POST request to a given URL with a given email.
-
-Usage: ./2-post_email.py <URL> <email>
-  - Displays the body of the response.
 """
 import sys
 import urllib.parse
