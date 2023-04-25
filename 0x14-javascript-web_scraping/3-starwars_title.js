ddb6c0d064e00c726d2dc1958fb5110691e165b6
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-04-25 23:16:30 +0300

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-
 const movieId = process.argv[2];
-
 if (parseInt(movieId) < 8) {
   const url = 'https://swapi-api.hbtn.io/api/films/' + movieId;
 
