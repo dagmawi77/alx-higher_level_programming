cf51cbff842e956a2c6efecb8292c03e7e5c54b7
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-04-25 23:18:59 +0300

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,22 @@
 #!/usr/bin/node
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (!error) {
-    const todos = JSON.parse(body);
-    let completed = {};
-    todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
-      }
-    });
-    console.log(completed);
+
+const requestURL = process.argv[2];
+const users = {};
+
+request(requestURL, function (err, response, body) {
+  if (err) {
+    return console.log(err);
   }
+
+  const pBody = JSON.parse(body);
+
+  for (let i = 0; i < pBody.length; i++) {
+    const tmp = pBody[i].userId;
+    if (pBody[i].completed) {
+      users[tmp] = (users[tmp] || 0) + 1;
+    }
+  }
+
+  console.log(users);
 });
